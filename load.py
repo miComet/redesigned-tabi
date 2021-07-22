a495f59f47a2371f5733622d88c7876b99145671
FATESAIKOU <qzsecftbhhhh@gmail.com>
2021-07-22 18:00:24 +0900

    Add workaround for PEP-8 to import app

--- load.py
+++ load.py
@@ -5,10 +5,12 @@ import os
 
 os.environ['ENV_CONFIG'] = 'LoadDataConfig'
 
-if __name__ == '__main__':
+# workaround for PEP-8
+if True:
     from src.app import app
+
+if __name__ == '__main__':
     with app.app_context():
         u = models.User(name='foo')
         db.session.add(u)
         db.session.commit()
-
--- run.py
+++ run.py
@@ -2,6 +2,9 @@ import os
 
 os.environ['ENV_CONFIG'] = 'DevelopmentConfig'
 
-if __name__ == '__main__':
+# workaround for PEP-8
+if True:
     from src.app import app
+
+if __name__ == '__main__':
     app.run()
