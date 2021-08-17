e2b4e7b4b2faa304f2f8a56da13e4acdc233ec53
FATESAIKOU <qzsecftbhhhh@gmail.com>
2021-08-18 00:39:09 +0900

    Add default STORAGE_SUBDIR to launch_db.sh

--- scripts/launch_db.sh
+++ scripts/launch_db.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export STORAGE_SUBDIR=storage_space/$(git rev-parse HEAD)-docker
+mkdir -p $STORAGE_SUBDIR
+
 cd $(dirname "$0")/../
 export PROJECT_DIR=$(pwd)
 docker-compose up db
